ab20442414023cb793c0076620f26cf805c973f0
hongkyuRyu <minsubrother@naver.com>
2024-01-17 03:25:30 +0900

    2024-01-17

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
 import './App.css';
+import BoardItem from 'components/BoardListItem';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { latestBoardListMock } from 'mocks';
 
 function App() {
   return (
-    <>Hello World!</>
+    <Router>
+      <Routes>
+        {latestBoardListMock.map((boardListItem, index) => (
+          <Route key={index} path={`/board/${index}`} element={<BoardItem boardListItem={boardListItem} />} />
+        ))}
+
+        <Route path="/" element={<div className='text-3xl font-bold underline'>홈 페이지입니다.</div>} />
+      </Routes>
+    </Router>
   );
 }
 
