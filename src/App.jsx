419f96165c5ec494049aa28491e5528f28831802
hongkyuRyu <minsubrother@naver.com>
2024-01-18 16:21:56 +0900

    2024-01-18

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BoardItem from 'components/BoardListItem';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { latestBoardListMock } from 'mocks';
+
+import SockJS from "sockjs-client/dist/sockjs"
+import { over } from "stompjs";
+
+
+
+const [stompClient, setStompClient] = useState();
+
+
+const connect = () => {
+
+  const sock = new SockJS("http://localhost:4000/ws")
+  const temp = over(sock);
+  setStompClient(temp);
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "X-XSRF-TOKEN": getCookie("XSRF-TOKEN")
+  }
+
+
+
+}
+
+
 
 function App() {
   return (
